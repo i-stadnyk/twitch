8b9409170037a5732e293e094e92e9266df3540d
Igor Stadnik <i.stadnik.dev@gmail.com>
2023-12-23 14:04:17 +0200

    Hydration error fix

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,4 +1,6 @@
-import Toggle from '@/app/(browse)/_components/sidebar/toggle'
+import Toggle, {
+	ToggleSkeleton
+} from '@/app/(browse)/_components/sidebar/toggle'
 import Wrapper from '@/app/(browse)/_components/sidebar/wrapper'
 import Recommended, {
 	RecommendedSkeleton
@@ -24,6 +26,7 @@ export default Sidebar
 export const SidebarSkeleton = () => {
 	return (
 		<aside className='fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50'>
+			<ToggleSkeleton />
 			<RecommendedSkeleton />
 		</aside>
 	)
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,13 +1,29 @@
 'use client'
 
+import { useIsClient } from 'usehooks-ts'
+
 import { cn } from '@/lib/utils'
 import { useSidebar } from '@/store/use-sidebar'
+
+import { ToggleSkeleton } from '@/app/(browse)/_components/sidebar/toggle'
+import { RecommendedSkeleton } from '@/app/(browse)/_components/sidebar/recommended'
 
 interface WrapperProps {
 	children: React.ReactNode
 }
 const Wrapper = ({ children }: WrapperProps) => {
+	const isClient = useIsClient()
 	const { collapsed } = useSidebar(state => state)
+
+	if (!isClient) {
+		return (
+			<aside className='fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50'>
+				<ToggleSkeleton />
+				<RecommendedSkeleton />
+			</aside>
+		)
+	}
+
 	return (
 		<aside
 			className={cn(
